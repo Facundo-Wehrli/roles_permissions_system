a94bd07ee6ba91ba3a0ce3c87ab09a5f1eab6a18
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-07-22 15:49:57 -0300

    feat: initial logic and persistence layer working

---
+++ src/main/java/com/facundowehrli/login/logic/User.java
@@ -0,0 +1,51 @@
+package com.facundowehrli.login.logic;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/com/facundowehrli/login/persistence/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.facundowehrli.login.persistence.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
