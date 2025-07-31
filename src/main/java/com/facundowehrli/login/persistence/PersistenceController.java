4afe3b4ceac6f949a65514a6ea25cff59d85d67a
Facundo Gabriel Wehrli <wehrlifacundo9@gmail.com>
2025-07-31 12:01:58 -0300

    refactor(persistence): prepare for role-related persistence logic

--- src/main/java/com/facundowehrli/login/persistence/PersistenceController.java
+++ src/main/java/com/facundowehrli/login/persistence/PersistenceController.java
@@ -6,10 +6,11 @@ import java.util.List;
 public class PersistenceController {
 
     UserJpaController userJpa = new UserJpaController();
+    RolJpaController rolJpa = new RolJpaController();   
 
     public List<User> getUsers() {
         //SELECT * FROM USERS
         return userJpa.findUserEntities();
-        
+
     }
 }
